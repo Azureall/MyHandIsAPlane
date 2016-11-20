3b12da620e1bbf1da84d4136be04d581a4166441
NickCrawford <ncrawfo7@kent.edu>
2016-11-20 06:45:28 -0500

    Fixes non  movement bug

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -7,9 +7,11 @@ public class GameController : MonoBehaviour {
 	public int worldX = 2000;
 	public int worldZ = 2000;
 
+	public Transform hoop;
+
 	// Use this for initialization
 	void Start () {
-
+		spawnHoop();
 	}
 	
 	// Update is called once per frame
@@ -18,6 +20,19 @@ public class GameController : MonoBehaviour {
 	}
 
 	void spawnHoop() {
+		float x, z;
+		x = (Random.value * worldX);
+		z = (Random.value * worldX);
+
+		//Generate y coordinate to make sure it stays above the terrain.
+		Vector3 pos = transform.position;
+		pos.x = x;
+		pos.z = z;
+		pos.y = Terrain.activeTerrain.SampleHeight(transform.position);
+
+		pos.y += Random.value * 175 + 25;
+
+		Instantiate(hoop, pos, Quaternion.identity);
 
 	}
 }
